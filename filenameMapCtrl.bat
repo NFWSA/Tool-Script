8b583b9cd6be2011ec42f1a16b98bf704ca02a68
SurgeNight <surgenight@hotmail.com>
2017-02-28 22:40:55 +0800

    Add two scripts about filename map.
    Details:
      1. filenameMapCtrl use a inline map to rename file with origin or
    record name;
      2. filenameMapGen collect files and make a half or full map with a few
    user-defined function.

---
+++ filenameMapCtrl.bat
@@ -0,0 +1,87 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem ---- user config start
+rem ori : use origin filename as filename
+rem rec : use record filename as filename
+set nameMode=ori
+rem use space split file type
+set fileType=png
+rem ---- user config end
+
+for %%a in (%fileType%) do (
+    set t=!t! *.%%a
+)
+set "fileType=%t%"
+for /f "tokens=1,2* delims=^|" %%a in ('more "%~dpnx0" +87') do (
+    set "name[%%a]=%%b"
+    set "tar[%%b]=%%a"
+)
+echo/
+echo            Warning: name cannot include / \ : ? " < > |
+echo    a. add filename map[rename file if needed]
+echo    c. change filename with record
+echo    r. recover filename
+choice /c acr /n /m "a, c, r ?"
+if %errorlevel% equ 1 goto :addMap
+if %errorlevel% equ 2 goto :chanName
+if %errorlevel% equ 3 goto :recovName
+echo Error!
+pause
+goto :EOF
+
+rem add filename map part
+:addMap
+cls
+for /f "delims=" %%a in ('dir /b %fileType%') do (
+    if "%nameMode%"=="ori" (
+        if "!name[%%~na]!"=="" (
+            set /p tarName=%%~nxa :  
+            if not "!tarName!"=="" (
+                >> "%~dpnx0" echo/%%~na^|!tarName!
+                echo file "%%~nxa" recorded.
+            ) else (
+                echo Null filename, so skip the file.
+            )
+        )
+    )
+    if "%nameMode%"=="rec" (
+        if "!tar[%%~na]!"=="" (
+            set /p tarName=%%~nxa :  
+            if not "!tarName!"=="" (
+                >> "%~dpnx0" echo/%%~na^|!tarName!
+                echo/"%%~nxa" = "!tarName!%%~xa"
+                rename "%%~nxa" "!tarName!%%~xa"
+            ) else (
+                echo Null filename, so skip the file.
+            )
+        )
+    )
+)
+pause
+goto :EOF
+
+rem change filename to record name
+:chanName
+cls
+for /f "delims=" %%a in ('dir /b %fileType%') do (
+    if not "!name[%%~na]!"=="" (
+        echo/"%%~nxa" = "!name[%%~na]!%%~xa"
+        rename "%%~nxa" "!name[%%~na]!%%~xa"
+    )
+)
+pause
+goto :EOF
+
+rem recover filename to origin name
+:recovName
+cls
+for /f "delims=" %%a in ('dir /b %fileType%') do (
+    if not "!tar[%%~na]!"=="" (
+        echo/"%%~nxa" = "!tar[%%~na]!%%~xa"
+        rename "%%~nxa" "!tar[%%~na]!%%~xa"
+    )
+)
+pause
+goto :EOF
+
---
+++ filenameMapGen.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem ---- user config start
+rem ori : use filename as origin filename
+rem rec : use filename as record filename
+set nameMode=rec
+rem use space split file type
+set fileType=png bat
+rem after get filename, before record it, user-defined function
+rem input arg: filename, the filename not include extension name
+rem input arg: extname, the extension name include "."
+rem output arg: filename, the filename will record to file
+rem output arg: inName, the filename will record to file that another part of complete record
+goto :main
+:afterGet
+
+goto :EOF
+rem ---- user config end
+
+:main
+for %%a in (%fileType%) do (
+    set t=!t! *.%%a
+)
+set "fileType=%t%"
+
+for /f "delims=" %%a in ('dir /b %fileType%') do (
+    set "filename=%%~na"
+    set "extname=%%~xa"
+    set "inName="
+    call :afterGet
+    if "%nameMode%"=="ori" (
+        >> "%~dpnx0" echo/!filename!^|!inName!
+    )
+    if "%nameMode%"=="rec" (
+        >> "%~dpnx0" echo/!inName!^|!filename!
+    )
+)
+pause
+goto :EOF
+
